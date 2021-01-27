4dc41af44ca15de33aaff1cdbe26e70cb593bbfc
Leonard Chin <leonard-chin@cookpad.com>
2021-01-27 10:05:42 +0900

    use mt to combine capslock with os key

--- keyboards/preonic/keymaps/l15n/keymap.c
+++ keyboards/preonic/keymaps/l15n/keymap.c
@@ -75,8 +75,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV,  KC_BSPC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
   KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_TAB,  KC_ESC,  KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_MINUS, KC_QUOT, KC_H,   KC_N,    KC_E,    KC_I,    KC_O,
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_CAPS, KC_ENT,  KC_K,    KC_M,    KC_COMM, KC_UP,   KC_DOT,
-  KC_LCTL, KC_LALT, KC_LGUI, MT(MOD_LSFT,KC_EQUAL), LOWER,   MT(MOD_LGUI,KC_SPC),  MT(MOD_LGUI,KC_SPC),  RAISE,   MT(MOD_RSFT,KC_SLSH), KC_LEFT, KC_DOWN, KC_RGHT
+  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    MT(MOD_LGUI,KC_CAPS), KC_ENT,  KC_K,    KC_M,    KC_COMM, KC_UP,   KC_DOT,
+  KC_LCTL, KC_LALT, KC_LGUI, MT(MOD_LSFT,KC_EQUAL), LOWER, KC_SPC,  KC_SPC,  RAISE,   MT(MOD_RSFT,KC_SLSH), KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 /* Dvorak
