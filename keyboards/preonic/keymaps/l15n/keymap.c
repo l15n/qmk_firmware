b2d5c589969fa519370be60e37e077f0030061a9
Leonard Chin <leonard-chin@cookpad.com>
2021-02-03 11:48:33 +0900

    Add more shift keys

--- keyboards/preonic/keymaps/l15n/keymap.c
+++ keyboards/preonic/keymaps/l15n/keymap.c
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV,  KC_BSPC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
   KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_TAB,  KC_ESC,  KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_MINUS, KC_QUOT, KC_H,   KC_N,    KC_E,    KC_I,    KC_O,
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_BSLS, KC_ENT,  KC_K,    KC_M,    KC_COMM, KC_UP,   KC_DOT,
+  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     MT(MOD_LSFT,KC_BSLS),  MT(MOD_RSFT,KC_ENT),  KC_K,    KC_M,    KC_COMM, KC_UP,   KC_DOT,
   RAISE,   KC_LALT, KC_LCTL, MT(MOD_LSFT,KC_EQUAL), MT(MOD_LGUI,KC_CAPS), KC_SPC,  KC_SPC,  LOWER,   MT(MOD_RSFT,KC_SLSH), KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
