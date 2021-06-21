25fc7ba3b9742176006de73ffd061360cd13f5d3
Leonard Chin <leonard-chin@cookpad.com>
2021-06-21 16:30:19 +0900

    Using hyper 1 to switch input method

--- keyboards/preonic/keymaps/l15n/keymap.c
+++ keyboards/preonic/keymaps/l15n/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+-------+-------+--------+-------+------+-------+------+------+------+------|
  * |   Q  |   W  |   F   |   P   |  Tab   |   \  |   G  |   J   |   L  |   U  |   Y  |   ;  |
  * |------+------+-------+-------+--------+--------------+-------+------+------+------+------|
- * |   A  |   R  |   S   |   T   |  caps   |   '   |   D  |   H   |   N  |   E  |   I  |   O  |
+ * |   A  |   R  |   S   |   T   |  hyper1   |   '   |   D  |   H   |   N  |   E  |   I  |   O  |
  * |------+------+-------+-------+--------+-------|------+-------+------+------+------+------|
  * |   Z  |   X  |   C   |   V   |  Esc   | Enter |   B  |   K   |   M  |   ,  |  Up  |   .  |
  * |------+------+-------+-------+--------+-------+------+-------+------+------+------+------|
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_COLEMAK] = LAYOUT_preonic_grid(
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV,  KC_BSPC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
   KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_TAB,  KC_BSLS,  KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
-  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_CAPS, KC_QUOT, KC_H,   KC_N,    KC_E,    KC_I,    KC_O,
+  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    HYPR(KC_1), KC_QUOT, KC_H,   KC_N,    KC_E,    KC_I,    KC_O,
   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    MT(MOD_LSFT,KC_ESC),  MT(MOD_RSFT,KC_ENT),  KC_K,    KC_M,    KC_COMM, KC_UP,   KC_DOT,
   RAISE,   KC_LALT, KC_LCTL, MT(MOD_LSFT,KC_EQUAL), KC_LGUI, KC_SPC,  KC_SPC, LOWER, MT(MOD_RSFT,KC_SLSH), KC_LEFT, KC_DOWN, KC_RGHT
 ),
