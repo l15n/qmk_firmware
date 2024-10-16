3b31c3037f23058c95f0fc0fc09e3c280d0e1d1d
Leonard Chin <3714+l15n@users.noreply.github.com>
2024-10-16 14:05:26 +0900

    Fix keycodes for lang, lang2

--- keyboards/preonic/keymaps/l15n/keymap.c
+++ keyboards/preonic/keymaps/l15n/keymap.c
@@ -117,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_MINUS,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-  BACKLIT, KC_F7,   KC_F8,   KC_F9,   KC_LANG2,  KC_F11,  KC_F12,KC_LANG1,S(KC_NUBS),KC_LBRC, KC_RBRC, KC_BSLS,
+  BACKLIT, KC_F7,   KC_F8,   KC_F9,   KC_LNG2,  KC_F11,  KC_F12, KC_LNG1,S(KC_NUBS),KC_LBRC, KC_RBRC, KC_BSLS,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
